a4c3f8c6e6d7ee5665a14c6f4c1c45eec89abe17
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2021-05-25 11:13:04 +1000

    Fix Variables, Parameters and tests for HBCS eligibility

--- openfisca_act_conveyancing/variables/home_buyer_concession_scheme.py
+++ openfisca_act_conveyancing/variables/home_buyer_concession_scheme.py
@@ -6,6 +6,7 @@ from openfisca_core.variables import Variable
 # Import the Entities specifically defined for this tax and benefit system
 from openfisca_act_conveyancing.entities import Household
 
+import numpy
 
 
 class PropertyType(Enum):
@@ -128,3 +129,41 @@ class eligible_for_home_buyer_concession(Variable):
         household_income_requirement_satisfied = household("household_income_requirement_satisfied", period)
 
         return all_buyers_are_18_years_old * residence_requirements_satisfied * property_requirements_satisfied * household_income_requirement_satisfied
+
+
+class conveyance_duty_under_HBCS(Variable):
+    value_type = float
+    entity = Household
+    definition_period = DAY
+    label = "Conveyance Duty Under HBCS"
+    reference = "https://www.revenue.act.gov.au/home-buyer-assistance/home-buyer-concession-scheme"  # Always use the most official source
+    default_value = 0
+    unit = "currency-AUD"
+
+    def formula(household, period, parameters):
+        """
+        Conveyance Duty Under the Home Buyers Concession Scheme.
+
+        The ACT Government has a concession scheme to help people buy a home or residential land. The scheme is administered by the ACT Revenue Office.
+        """
+
+        property_is_new = household("property_type", period) == PropertyType.new_home
+        property_is_vacant_land = household("property_type", period) == PropertyType.vacant_land
+
+        property_price = household("property_price", period)
+
+        conveyance_HBCS_new = parameters(period).home_buyer_concession_scheme.conveyance_duty_schedule.new_home.calc(property_price)
+        conveyance_HBCS_vacant_land = parameters(period).home_buyer_concession_scheme.conveyance_duty_schedule.vacant_land.calc(property_price)
+
+        return numpy.select(
+            [property_is_new, property_is_vacant_land], [conveyance_HBCS_new, conveyance_HBCS_vacant_land]
+        )
+
+    def formula_2019_07_01(household, period, parameters):
+        """
+        Conveyance Duty Under the Home Buyers Concession Scheme.
+
+        The ACT Government has a concession scheme to help people buy a home or residential land. The scheme is administered by the ACT Revenue Office.
+        """
+
+        return 0
