8fb331c3e974ac0d5a80f478fe37b208d8caecbd
Ram Parameswaran <ramanan.parameswaran@gmail.com>
2021-05-30 00:20:03 +1000

    Add functionality to calculate HBCS eligibility on frontend

--- conveyance_calculator/src/api/openfisca_api.js
+++ conveyance_calculator/src/api/openfisca_api.js
@@ -32,8 +32,43 @@ function calculate_conveyance_duty(date, property_price, land_use_type ) {
   });
 }
 
+function calculate_HBCS_eligibility(date, all_buyers_are_18_years_old, residence_requirements_satisfied, property_requirements_satisfied, household_income, number_dependent_children, eligible_for_home_buyer_concession ) {
+  return OpenFiscaApiBase({
+    url: '/calculate/',
+    method: 'POST',
+    data: {
+      "persons": {
+        "person 1": {}
+      },
+      "households": {
+        "household_1": {
+          "eligible_for_home_buyer_concession": {
+            [date]: eligible_for_home_buyer_concession
+          },
+          "all_buyers_are_18_years_old": {
+            [date]: all_buyers_are_18_years_old
+          },
+          "residence_requirements_satisfied": {
+            [date]: residence_requirements_satisfied
+          },
+          "property_requirements_satisfied": {
+            [date]: property_requirements_satisfied
+          },
+          "household_income": {
+            [date]: household_income
+          },
+          "number_dependent_children": {
+            [date]: number_dependent_children
+          }
+        }
+      }
+    },
+  });
+}
+
 const OpenFiscaApi = {
-  calculate_conveyance_duty
+  calculate_conveyance_duty,
+  calculate_HBCS_eligibility
 };
 
 export default OpenFiscaApi;
--- conveyance_calculator/src/components/App.js
+++ conveyance_calculator/src/components/App.js
@@ -5,38 +5,58 @@ import OpenFiscaApi from '../api/openfisca_api';
 import moment from 'moment';
 import {
   Button,
+  Card,
+  CardContent,
   FormControl,
+  FormControlLabel,
+  FormGroup,
+  Grid,
+  InputAdornment,
   InputLabel,
   MenuItem,
-  Paper,
+  Radio,
+  RadioGroup,
   Select,
-  Step,
-  StepContent,
-  StepLabel,
-  Stepper,
+  Switch,
   TextField,
   Typography,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import Alert from '@material-ui/lab/Alert';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
   },
+  card: {
+    minWidth: 575,
+  },
+
+  actionsContainer: {
+    marginBottom: theme.spacing(2),
+    textAlign: 'center',
+  },
   button: {
     marginTop: theme.spacing(1),
     marginRight: theme.spacing(1),
+    width: '40%',
   },
-  actionsContainer: {
-    marginBottom: theme.spacing(2),
-  },
+
   resetContainer: {
     padding: theme.spacing(3),
   },
   formControl: {
     display: 'block',
-    margin: theme.spacing(1),
-    minWidth: 120,
+    margin: theme.spacing(3),
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+  input: {
+    width: '100%',
+  },
+  toggleSwitch: {
+    marginTop: 15,
+    marginBottom: 15,
   },
 }));
 
@@ -45,11 +65,49 @@ const ActiveStepContent = (props) => {
 
   const { activeStep, handleBack, handleNext, openFiscaState, setOpenFiscaState } = props;
 
+  const [showHBSCquestions, setShowHBSCquestions] = useState(false);
+
+  const handleCalculate = () => {
+    // Calculate conveyance duties payable
+    OpenFiscaApi.calculate_conveyance_duty(
+      openFiscaState.date,
+      openFiscaState.property_price,
+      openFiscaState.land_use_type,
+    ).then((res) => {
+      setOpenFiscaState((openFiscaState) => ({
+        ...openFiscaState,
+        conveyance_duty: res.data.households.household_1.conveyance_duty[openFiscaState.date],
+        conveyance_duty_under_HBCS:
+          res.data.households.household_1.conveyance_duty_under_HBCS[openFiscaState.date],
+      }));
+    });
+
+    // Calculate HBCS eligibility
+    OpenFiscaApi.calculate_HBCS_eligibility(
+      openFiscaState.date,
+      openFiscaState.all_buyers_are_18_years_old,
+      openFiscaState.residence_requirements_satisfied,
+      openFiscaState.property_requirements_satisfied,
+      openFiscaState.household_income,
+      openFiscaState.number_dependent_children,
+      openFiscaState.eligible_for_home_buyer_concession,
+    ).then((res) => {
+      setOpenFiscaState((openFiscaState) => ({
+        ...openFiscaState,
+        eligible_for_home_buyer_concession:
+          res.data.households.household_1.eligible_for_home_buyer_concession[openFiscaState.date],
+      }));
+      setShowHBSCquestions(false);
+    });
+  };
+
   switch (activeStep) {
     case 0:
       return (
         <div>
-          <Typography variant="h6">Enter property details</Typography>
+          <Typography variant="h5" align="center" gutterBottom>
+            Conveyance Duty Calculator
+          </Typography>
           <FormControl className={classes.formControl}>
             <TextField
               id="purchase_date"
@@ -65,6 +123,7 @@ const ActiveStepContent = (props) => {
               InputLabelProps={{
                 shrink: true,
               }}
+              className={classes.input}
             />
           </FormControl>
 
@@ -77,20 +136,24 @@ const ActiveStepContent = (props) => {
               onChange={(e) =>
                 setOpenFiscaState((openFiscaState) => ({
                   ...openFiscaState,
-                  property_price: e.target.value,
+                  property_price: parseFloat(e.target.value),
                 }))
               }
               InputLabelProps={{
                 shrink: true,
               }}
+              InputProps={{
+                startAdornment: <InputAdornment position="start">$</InputAdornment>,
+              }}
+              className={classes.input}
             />
           </FormControl>
 
           <FormControl className={classes.formControl}>
-            <InputLabel id="demo-simple-select-label">Age</InputLabel>
+            <InputLabel id="land_use_type">Land use type</InputLabel>
             <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
+              labelId="land-use-type"
+              id="land-use-type-select"
               value={openFiscaState.land_use_type}
               onChange={(e) =>
                 setOpenFiscaState((openFiscaState) => ({
@@ -98,6 +161,7 @@ const ActiveStepContent = (props) => {
                   land_use_type: e.target.value,
                 }))
               }
+              className={classes.input}
             >
               <MenuItem value="residential">Residential</MenuItem>
               <MenuItem value="rural">Rural</MenuItem>
@@ -107,7 +171,12 @@ const ActiveStepContent = (props) => {
 
           <div className={classes.actionsContainer}>
             <div>
-              <Button disabled={activeStep === 0} onClick={handleBack} className={classes.button}>
+              <Button
+                disabled={activeStep === 0}
+                onClick={handleBack}
+                className={classes.button}
+                size="large"
+              >
                 Back
               </Button>
               <Button
@@ -115,6 +184,7 @@ const ActiveStepContent = (props) => {
                 color="primary"
                 onClick={handleNext}
                 className={classes.button}
+                size="large"
               >
                 Next
               </Button>
@@ -126,20 +196,145 @@ const ActiveStepContent = (props) => {
     case 1:
       return (
         <div>
-          <Typography variant="h6">Are you eligible for the HBCS?</Typography>
+          <Typography variant="h5" align="center" gutterBottom>
+            Are you eligible for the HBCS?
+          </Typography>
+
+          <FormControl className={classes.formControl}>
+            <RadioGroup
+              aria-label="eligible_HBSC"
+              name="eligible_HBSC"
+              value={openFiscaState.eligible_for_home_buyer_concession}
+              onChange={(e) => {
+                if (e.target.value) {
+                  setOpenFiscaState((openFiscaState) => ({
+                    ...openFiscaState,
+                    eligible_for_home_buyer_concession: e.target.value === 'true',
+                  }));
+                  setShowHBSCquestions(false);
+                } else {
+                  setOpenFiscaState((openFiscaState) => ({
+                    ...openFiscaState,
+                    eligible_for_home_buyer_concession: null,
+                  }));
+                  setShowHBSCquestions(true);
+                }
+              }}
+            >
+              <FormControlLabel value={false} control={<Radio />} label="No" />
+              <FormControlLabel value={true} control={<Radio />} label="Yes" />
+              <FormControlLabel value={null} control={<Radio />} label="I don't know" />
+            </RadioGroup>
+
+            {showHBSCquestions && (
+              <div style={{ paddingTop: 30 }}>
+                <FormGroup className={classes.toggleSwitch}>
+                  <FormControlLabel
+                    control={<Switch color="primary" />}
+                    label="All buyers at least 18 years old"
+                    checked={openFiscaState.all_buyers_are_18_years_old}
+                    onChange={(e) => {
+                      setOpenFiscaState((openFiscaState) => ({
+                        ...openFiscaState,
+                        all_buyers_are_18_years_old: e.target.checked,
+                      }));
+                    }}
+                  />
+                </FormGroup>
+
+                <FormGroup className={classes.toggleSwitch}>
+                  <FormControlLabel
+                    control={<Switch color="primary" />}
+                    label="At least one buyer live on the property for one year"
+                    checked={openFiscaState.residence_requirements_satisfied}
+                    onChange={(e) => {
+                      setOpenFiscaState((openFiscaState) => ({
+                        ...openFiscaState,
+                        residence_requirements_satisfied: e.target.checked,
+                      }));
+                    }}
+                  />
+                </FormGroup>
+
+                <FormGroup className={classes.toggleSwitch}>
+                  <FormControlLabel
+                    control={<Switch color="primary" />}
+                    label="No buyer has owned a property in the last two years"
+                    checked={openFiscaState.property_requirements_satisfied}
+                    onChange={(e) => {
+                      setOpenFiscaState((openFiscaState) => ({
+                        ...openFiscaState,
+                        property_requirements_satisfied: e.target.checked,
+                      }));
+                    }}
+                  />
+                </FormGroup>
+
+                <FormGroup className={classes.toggleSwitch}>
+                  <TextField
+                    id="household_income"
+                    label="Household gross income"
+                    type="number"
+                    value={openFiscaState.household_income}
+                    onChange={(e) =>
+                      setOpenFiscaState((openFiscaState) => ({
+                        ...openFiscaState,
+                        household_income: parseFloat(e.target.value),
+                      }))
+                    }
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    InputProps={{
+                      startAdornment: <InputAdornment position="start">$</InputAdornment>,
+                    }}
+                    className={classes.input}
+                  />
+                </FormGroup>
+
+                <FormGroup className={classes.toggleSwitch}>
+                  <TextField
+                    id="dependent_children"
+                    label="Number of dependent children"
+                    type="number"
+                    value={openFiscaState.number_dependent_children}
+                    onChange={(e) =>
+                      setOpenFiscaState((openFiscaState) => ({
+                        ...openFiscaState,
+                        number_dependent_children: parseInt(e.target.value),
+                      }))
+                    }
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    className={classes.input}
+                  />
+                </FormGroup>
+              </div>
+            )}
+          </FormControl>
 
           <div className={classes.actionsContainer}>
             <div>
-              <Button disabled={activeStep === 0} onClick={handleBack} className={classes.button}>
+              <Button
+                disabled={activeStep === 0}
+                onClick={handleBack}
+                className={classes.button}
+                size="large"
+              >
                 Back
               </Button>
               <Button
                 variant="contained"
                 color="primary"
-                onClick={handleNext}
+                onClick={(e) => {
+                  handleCalculate();
+                  handleNext();
+                }}
                 className={classes.button}
+                size="large"
               >
-                Next
+                Calculate
               </Button>
             </div>
           </div>
@@ -149,17 +344,34 @@ const ActiveStepContent = (props) => {
     case 2:
       return (
         <div>
-          <Typography variant="h6">See results</Typography>
-          {openFiscaState.conveyance_duty !== null && (
-            <Fragment>
-              <Typography variant="h6">
-                Conveyance duty payable: ${openFiscaState.conveyance_duty}
-              </Typography>
-              <Typography variant="h6">
-                Conveyance duty (after concession): ${openFiscaState.conveyance_duty_under_HBCS}
-              </Typography>
-            </Fragment>
-          )}
+          <Typography variant="h5" align="center" gutterBottom>
+            See results
+          </Typography>
+
+          <FormControl className={classes.formControl}>
+            <Typography variant="h6" gutterBottom>
+              Conveyance duty payable: ${openFiscaState.conveyance_duty}
+            </Typography>
+            <Typography
+              variant="h6"
+              style={
+                openFiscaState.eligible_for_home_buyer_concession
+                  ? {}
+                  : {
+                      textDecoration: 'line-through',
+                    }
+              }
+            >
+              Conveyance duty (after concession): ${openFiscaState.conveyance_duty_under_HBCS}
+            </Typography>
+            {!openFiscaState.eligible_for_home_buyer_concession && (
+              <Alert severity="warning">
+                Based on the information provided, you are not eligible for the Home Buyer
+                Concession Scheme!
+              </Alert>
+            )}
+          </FormControl>
+
           <div className={classes.actionsContainer}>
             <div>
               <Button disabled={activeStep === 0} onClick={handleBack} className={classes.button}>
@@ -169,25 +381,11 @@ const ActiveStepContent = (props) => {
                 variant="contained"
                 color="primary"
                 onClick={(e) => {
-                  OpenFiscaApi.calculate_conveyance_duty(
-                    openFiscaState.date,
-                    openFiscaState.property_price,
-                    openFiscaState.land_use_type,
-                  ).then((res) => {
-                    setOpenFiscaState((openFiscaState) => ({
-                      ...openFiscaState,
-                      conveyance_duty:
-                        res.data.households.household_1.conveyance_duty[openFiscaState.date],
-                      conveyance_duty_under_HBCS:
-                        res.data.households.household_1.conveyance_duty_under_HBCS[
-                          openFiscaState.date
-                        ],
-                    }));
-                  });
+                  handleCalculate();
                 }}
                 className={classes.button}
               >
-                Calculate
+                Calculate again
               </Button>
             </div>
           </div>
@@ -209,6 +407,12 @@ function App() {
     conveyance_duty_under_HBCS: null,
     property_price: 0,
     land_use_type: 'residential',
+    eligible_for_home_buyer_concession: false,
+    all_buyers_are_18_years_old: false,
+    residence_requirements_satisfied: false,
+    property_requirements_satisfied: false,
+    household_income: 0,
+    number_dependent_children: 0,
   });
 
   // STEPPER
@@ -222,15 +426,28 @@ function App() {
   };
 
   return (
-    <div className={classes.root}>
-      <ActiveStepContent
-        activeStep={activeStep}
-        handleBack={handleBack}
-        handleNext={handleNext}
-        openFiscaState={openFiscaState}
-        setOpenFiscaState={setOpenFiscaState}
-      />
-    </div>
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justify="center"
+      style={{ minHeight: '100vh' }}
+    >
+      <Grid item xs={5}>
+        <Card className={classes.card}>
+          <CardContent>
+            <ActiveStepContent
+              activeStep={activeStep}
+              handleBack={handleBack}
+              handleNext={handleNext}
+              openFiscaState={openFiscaState}
+              setOpenFiscaState={setOpenFiscaState}
+            />
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
   );
 }
 
